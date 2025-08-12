16147e154e9f568b58b0e41ff26f1edd837d3549
momy-123 <ndolane.diouf89@gmail.com>
2025-08-12 13:41:30 +0000

    Create app_iris.py

---
+++ app_iris.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import joblib
+import numpy as np
+del = joblib.load("iris_model.pkl")
+
+st.title("Prediction de l'espèce Iris")
+st.write("Entrz les caractéristiques de la fleur pour prédire son espèce")
+
+sepal_length = st.slider("La longueur du sepale (cm)", 4.0,8.0,5.0)
+sepal_width = st.slider("La largeur du sepale (cm)", 2.0,4.5,3.0)
+petal_length = st.slider("La longueur du sepale (cm)", 1.0,7.0,4.0)
+petal_width = st.slider("La largeur du sepale (cm)", 0.1,2.5,1.0)
+
+input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+if st.button("prédire"):
+    prediction = model.predict(input_data)[0]
+    species = ['Setosa', 'Versicolor', 'Verginica']
+    st.success(f"L'espèce prédite est: {species[prediction]}")
